6f93878509aec37d09cb2f0615d50b42d19a0bdb
bharat404606 <lalji.java606@gmail.com>
2025-01-17 10:41:20 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("This is Java Programm");
+        System.out.println("Output from  Java Programm");
+        System.out.println(new Date());
+        System.out.println("Hello");
+
+    }
+}
